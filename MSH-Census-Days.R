f6b405d3b822da69c8ea42cf79a7b4c24e68f7ea
greglenane <gregory.lenane@mssm.edu>
2021-05-24 09:32:34 -0400

    April 2021

--- MSH-Census-Days.R
+++ MSH-Census-Days.R
@@ -115,5 +115,5 @@ trend <- master()
 #Upload multiple files if necessary
 
 #start and end should be start and end of what you want to upload
-census_export <- upload(start = "02/28/2021",end = "03/27/2021")
+census_export <- upload(start = "03/28/2021",end = "04/24/2021")
 save()
